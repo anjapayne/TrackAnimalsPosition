bdfd283c7af17d873ac1754da555b1d0bbee1782
anjapayne <anjaelisahiggins@gmail.com>
2017-09-02 16:49:49 -0700

    Process of Shifting Video
    Adds a function to read in the shifted video index

---
+++ shiftVideo.m
@@ -0,0 +1,11 @@
+function video = shiftVideo(shiftedIndex); 
+% Script to shift the video frames so that they align with the ephys data
+% Written on September 2, 2017
+% Last modified by Anja Payne
+
+% Load the index data that was provided by Kitchen Sync
+fileList = dir('*index.txt'); 
+video = textread(shiftedIndex);
+%video = video(video>0); 
+
+end
